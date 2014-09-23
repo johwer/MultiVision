8c8e47db22a8b508b76cdf26a9bc08103b8a1d81
Johan Wergelius <johanwergelius@gmail.com>
2014-09-23 16:58:55 +0200

    Angular upp and running and Express gets data with Mongoose from MongoDB. Rendering html with Jade

---
+++ server.js
@@ -0,0 +1,78 @@
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+    nib = require('nib');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib());
+}
+app.set('title', 'My MEAN site');
+app.get('title');
+console.log(app.get('title'));
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+//app.use(bodyParser());
+
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        dest: __dirname + '/public',
+        debug: true,
+        force: true,
+        compile: compile
+    }
+));
+
+app.use(express.static(__dirname + '/public'));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function() {
+    console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc){
+    mongoMessage = messageDoc.message;
+});
+
+
+app.get('/partials/:partialPath', function (reg, res) {
+    res.render('partials/' + reg.params.partialPath);
+});
+
+//app.get('/');
+app.get('*', function(reg,res){
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+var port = 3030;
+app.listen(port);
+
+console.log('Listening on port' +port + '...');
